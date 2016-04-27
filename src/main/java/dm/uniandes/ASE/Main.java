af98126423c3e4efc98e05b1b594bea211a0943e
DavidMtz <df.martinez1@uniandes.edu.co>
2016-04-27 14:44:42 -0500

    eso

--- src/main/java/dm/uniandes/ASE/Main.java
+++ src/main/java/dm/uniandes/ASE/Main.java
@@ -113,22 +113,10 @@ public class Main {
 			return new ModelAndView(attributes, "test4.ftl");
 		}, new FreeMarkerEngine());
 		
-		String mainPath = new File(".").getAbsolutePath().toString();
 		
-		HashMap<String, List<Double>> data1 = new HashMap<String, List<Double>>();
-		try {
-			data1 = FileFinder.getPairData(mainPath.replace(".", filePath1));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 		HashMap<String, Double> result1 = new HashMap<String, Double>();
-		result1=Statistics.linearRegression(data1.get("x"), data1.get("y"));
-		
-		
+		result1 = algo(filePath1);
 		String a1 = result1.get("rXY").toString();
 		String a2 = result1.get("r2").toString();
 		String a3 = "";
@@ -144,50 +132,27 @@ public class Main {
 			attributes.put("A1", a1);
 			attributes.put("A2", a2);
 			attributes.put("A3", a3);
-//			attributes.put("A4", a4);
-//			attributes.put("A5", a5);
-//			attributes.put("A6", a6);
-//			attributes.put("A7", a7);
-//			attributes.put("A8", a8);
-//			attributes.put("A9", a9);
+			attributes.put("A4", a4);
+			attributes.put("A5", a5);
+			attributes.put("A6", a6);
+			attributes.put("A7", a7);
+			attributes.put("A8", a8);
+			attributes.put("A9", a9);
 			return new ModelAndView(attributes, "result1.ftl");
 		}, new FreeMarkerEngine());
-		
-		HashMap<String, List<Double>> data2 = new HashMap<String, List<Double>>();
-		try {
-			data2 = FileFinder.getPairData(mainPath.replace(".", filePath2));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		HashMap<String, Double> result2 = new HashMap<String, Double>();
-		result1=Statistics.linearRegression(data2.get("x"), data2.get("y"));
-		
-		String b1 = result2.get("rXY").toString();
-		String b2 = result2.get("r2").toString();
-		String b3 = "";
-		String b4 = result2.get("beta0").toString();
-		String b5 = result2.get("beta1").toString();
-		String b6 = result2.get("yK").toString();
-		String b7 = "";
-		String b8 = "";
-		String b9 = "";
 		
 		
 		get("/result2", (request, response) -> {
 			Map<String, Object> attributes = new HashMap<>();
-			attributes.put("B1", b1);
-			attributes.put("B2", b2);
-			attributes.put("B3", b3);
-			attributes.put("B4", b4);
-			attributes.put("B5", b5);
-			attributes.put("B6", b6);
-			attributes.put("B7", b7);
-			attributes.put("B8", b8);
-			attributes.put("B9", b9);
+			attributes.put("B1", "");
+			attributes.put("B2", "");
+			attributes.put("B3", "");
+			attributes.put("B4", "");
+			attributes.put("B5", "");
+			attributes.put("B6", "");
+			attributes.put("B7", "");
+			attributes.put("B8", "");
+			attributes.put("B9", "");
 			return new ModelAndView(attributes, "result2.ftl");
 		}, new FreeMarkerEngine());
 		
@@ -248,6 +213,24 @@ public class Main {
 	public static Double getUpperLimit (Double pP, Integer pDof){
 		Integral integral = new Integral(1.0, pDof);
 		return integral.findUpperLimit( pP);
+	}
+	
+	public static HashMap<String, Double> algo (String filePath){
+		
+		HashMap<String, List<Double>> data = new HashMap<String, List<Double>>();
+		try {
+			data = FileFinder.getPairData(filePath);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		HashMap<String, Double> result = new HashMap<String, Double>();
+		result=Statistics.linearRegression(data.get("x"), data.get("y"));
+		
+		return result;
 	}
 
 }
--- src/test/java/dm/uniandes/ASE/MainTest.java
+++ src/test/java/dm/uniandes/ASE/MainTest.java
@@ -14,26 +14,9 @@ public class MainTest {
 	 */
 	@Test
 	public void test1() {
-		Double resultado = Main.getUpperLimit(0.20, 6);
+		Double resultado = Main.algo("test1.txt").get("rXY");
 		assertEquals("La Integral deberia ser 0.55338", 0.55338, resultado, 1e-2);
 	}
 
-	/**
-	 * Metodo principal Prueba 2.
-	 */
-	@Test
-	public void test2() {
-		Double resultado = Main.getUpperLimit(0.45, 15);
-		assertEquals("La Integral deberia ser 1.75305", 1.75305, resultado, 1e-2);
-	}
-	
-	/**
-	 * Metodo principal Prueba 3.
-	 */
-	@Test
-	public void test3() {
-		Double resultado = Main.getUpperLimit(0.495 , 4);
-		assertEquals("La Integral deberia ser 4.60409", 4.60409, resultado, 1e-2);
-	}
 
 }
